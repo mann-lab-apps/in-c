be16de239d4e77c12f1d5af2ecf1ab954e4b51a0
mannMae <daga4242@gmail.com>
2026-08-29 19:14:31 +0900

    chore: bump Clef Android version code to 8

--- apps/in_c_sheet/lib/main.dart
+++ apps/in_c_sheet/lib/main.dart
@@ -35,7 +35,7 @@ import 'sheet_viewer_file_status.dart';
 import 'sheet_viewer_input.dart';
 
 const MethodChannel _sharedImportChannel = MethodChannel('clef/shared_imports');
-const String _clefAppVersion = '1.0.0+7';
+const String _clefAppVersion = '1.0.0+8';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
