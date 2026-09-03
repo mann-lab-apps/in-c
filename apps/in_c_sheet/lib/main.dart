2bc73abb199e95e02b41daa4495d7be058c07631
mannMae <daga4242@gmail.com>
2026-09-03 15:09:10 +0900

    chore: apply Darezzo C clef app icon

--- apps/in_c_sheet/lib/main.dart
+++ apps/in_c_sheet/lib/main.dart
@@ -38,7 +38,7 @@ import 'sheet_viewer_file_status.dart';
 import 'sheet_viewer_input.dart';
 
 const MethodChannel _sharedImportChannel = MethodChannel('clef/shared_imports');
-const String _clefAppVersion = '1.0.0+14';
+const String _clefAppVersion = '1.0.0+15';
 const bool _launchInCDiscoveryHome = bool.fromEnvironment(
   'IN_C_DISCOVERY_HOME',
 );
